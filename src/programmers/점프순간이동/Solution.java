464b27d4c2b493e8eac6adfaf733302ee1d7ec7d
sequaa <asl0619@naver.com>
2026-05-09 23:11:03 +0900

    Create programmers/점프순간이동/Solution.java

---
+++ src/programmers/점프순간이동/Solution.java
@@ -0,0 +1,19 @@
+package programmers.점프순간이동;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                n--;
+                ans++;
+            }
+            n /= 2;
+        }
+
+        return ans;
+    }
+}
